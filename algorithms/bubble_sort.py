1ea29ed7ccc482647f749959f8917270b6b54255
Eric Montague <eric.g.montague@gmail.com>
2020-04-12 11:28:12 -0400

    renamed variables to be more descriptive

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,15 +1,15 @@
 """This module contains my implementation of bubble sort."""
 
 
-def bubble_sort(input_list):
+def bubble_sort(unsorted_list):
     """Implementation of bubble sort. Bubble sort is 
     a stable sorting algorithm that sorts an 
     unsorted list of elements in-place."""
-    for i in range(len(input_list) - 1):
+    for i in range(len(unsorted_list) - 1):
         swapped = False
-        for j in range(len(input_list) - i - 1):
-            if input_list[j] > input_list[j + 1]:
-                input_list[j], input_list[j + 1] = input_list[j + 1], input_list[j]
+        for j in range(len(unsorted_list) - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
                 swapped = True
         if not swapped:
             break
