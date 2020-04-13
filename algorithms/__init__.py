7d522ff9ac6f2f8503d4007153348f228ba1c621
Eric Montague <eric.g.montague@gmail.com>
2020-04-13 12:20:01 -0400

    added import statements and docstring

---
+++ algorithms/__init__.py
@@ -0,0 +1,7 @@
+"""This package contains various sorting alorithms."""
+
+
+from algorithms.bubble_sort import bubble_sort
+from algorithms.selection_sort import selection_sort
+from algorithms.insertion_sort import insertion_sort
+from algorithms.merge_sort import merge_sort
