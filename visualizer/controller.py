5fe2198208a4458ee06e2019e2ec06887cf76031
Eric Montague <eric.g.montague@gmail.com>
2020-05-05 23:26:16 -0400

    wrote pseudo code for the run method and deleted the quit method

--- visualizer/controller.py
+++ visualizer/controller.py
@@ -1,6 +1,7 @@
 """This module contains the controller class for the sorting visualizer GUI."""
 
 
+import sys
 import pygame
 
 
@@ -12,11 +13,32 @@ class SortingVisualizerController:
 
     def run(self):
         """Start the visualizer."""
-        pass
-
-    def quit(self):
-        """Close pygame window and stop the program."""
-        pass
-
-
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if user clicked screen:
+                    if navbar was clicked:
+                        element = get_clicked_element()
+                        if element is a sorting label:
+                            #represents the button being selected. 
+                            #Maybe it shoud maintain state as to whether is was clicked?
+                            change_sorting_label_color() 
+                            redraw_interface()
+                        elif element is the one for changing array size:
+                            change_array_size()
+                            redraw_interface()
+                        elif element is the on for changing sorting speed:
+                            update_sorting_speed() #controller can maintain this. No redrawing necessary
+                    else: #either the sort, reset button, or somewhere else on the screen were clicked
+                        if sort button was clicked
+                            if one of the sorting labels is highlighted
+                                sort_bars_based_on_algorithm_selected()
+                            else:
+                                display_error_message() #need to select a sorting algorithm
+                                redraw_interface()
+                        elif reset button was clicked
+                            generate_a_new_array() #reset the whole thing
+                            redraw_interface()
+                            
 
