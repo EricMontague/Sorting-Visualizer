e440e6c1705ea9965e87b059011c324797f61463
Eric Montague <eric.g.montague@gmail.com>
2020-05-05 23:26:31 -0400

    removed navbar from view module

--- visualizer/views.py
+++ visualizer/views.py
@@ -2,21 +2,6 @@
 
 
 import pygame
-from visualizer.rectangles import Rectangle
-from visualizer.buttons import Button
-
-
-#needs buttons, and a rectangle
-class NavigationBar:
-    """Class to represent a navigation bar for the GUI."""
-
-    def __init__(self, background_color=(0, 0, 0)):
-        #background color defaults to black
-        self.background_color = background_color
-
-    def draw(self):
-        """Draw the navigation bar onto the screen."""
-        pass
 
 
 class SortingVisualizerGUI:
@@ -36,7 +21,7 @@ class SortingVisualizerGUI:
         self.width = width
         self.height = height
         self.background_color = background_color
-        self._navigation_bar = navigation_bar
-        self._navigation_bar.draw()
+        self.navigation_bar = navigation_bar
+        self.navigation_bar.draw()
         pygame.display.flip()
 
