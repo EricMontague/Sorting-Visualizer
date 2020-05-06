61b5603dcfbc7aa427d5d1091bedcaed2f609f6a
Eric Montague <eric.g.montague@gmail.com>
2020-05-06 12:28:32 -0400

    added method to the GUI class

--- visualizer/views.py
+++ visualizer/views.py
@@ -9,7 +9,9 @@ class SortingVisualizerGUI:
 
     def __init__(
         self,
-        navigation_bar,
+        navbar,
+        buttons,
+        bars,
         width=1080,
         height=600,
         background_color=(255, 255, 255),
@@ -21,7 +23,32 @@ class SortingVisualizerGUI:
         self.width = width
         self.height = height
         self.background_color = background_color
-        self.navigation_bar = navigation_bar
-        self.navigation_bar.draw()
+        self.navbar = navbar
+        self.navbar.draw()
+        self.buttons = buttons
+        self.bars = bars
+        for button in self.buttons:
+            button.draw()
+        for bar in self.bars:
+            bar.draw()
         pygame.display.flip()
+
+    def get_clicked_element(self):
+        """Return the element on the screen that was clicked by the user."""
+        pass
+
+    def update(self, element):
+        """Redraw the given element on the screen."""
+        pass
+
+    def display_message(self, message, coordinates):
+        """Display the given message on the screen."""
+        pass
+
+    def quit(self):
+        """Return True if the user has closed the pygame window."""
+        for event in pygame.event.get(eventtype=pygame.QUIT):
+            if event is not None:
+                return True
+        return False
 
