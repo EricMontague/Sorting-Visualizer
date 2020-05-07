5849fc02a47479e0f77fd3e2644706ebe6e6ad14
Eric Montague <eric.g.montague@gmail.com>
2020-05-07 16:45:39 -0400

    added new methods to GUI class

--- visualizer/views.py
+++ visualizer/views.py
@@ -45,10 +45,23 @@ class SortingVisualizerGUI:
         """Display the given message on the screen."""
         pass
 
+    def swap(self, bar_one, bar_two):
+        """Swap both of the given bars' positions."""
+        #both bars will be red when this method is called.
+        #first, swap them
+        #next, turn them green to indicate they've been swapped
+        pass
+
+    def sorting_finish(self):
+        """Change the colors of all of the bars to indicate
+        that they are sorted.
+        """
+        #change them to green first and then to purple
+        pass
+
     def quit(self):
         """Return True if the user has closed the pygame window."""
         for event in pygame.event.get(eventtype=pygame.QUIT):
             if event is not None:
                 return True
         return False
-
