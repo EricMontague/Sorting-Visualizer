aeefae9c21704a2c1366452af3d9edf2145a6159
Eric Montague <eric.g.montague@gmail.com>
2020-05-07 16:41:29 -0400

    added dictionary to be used for importing the sorting algorithms

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -7,3 +7,17 @@ from algorithms.bubble_sort import bubble_sort
 from algorithms.selection_sort import selection_sort
 from algorithms.insertion_sort import insertion_sort
 from algorithms.merge_sort import merge_sort
+from algorithms.quicksort import quicksort
+from algorithms.heapsort import heapsort
+
+
+ALGORITHMS = {
+    "Bubble Sort": bubble_sort,
+    "Selection Sort": selection_sort,
+    "Insertion Sort": insertion_sort,
+    "Merge Sort": merge_sort,
+    "Quicksort": quicksort,
+    "Heapsort": heapsort
+}
+
+
