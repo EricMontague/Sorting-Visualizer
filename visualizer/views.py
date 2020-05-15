1618f2b46cef86eb033f3151aa9f72fc42c9f2a7
Eric Montague <eric.g.montague@gmail.com>
2020-05-15 00:08:54 -0400

    removed the processing of building the pygame screen from the constructor

--- visualizer/views.py
+++ visualizer/views.py
@@ -7,30 +7,16 @@ import pygame
 class SortingVisualizerGUI:
     """Class to represent the main GUI view for the visualizer."""
 
-    def __init__(
-        self,
-        navbar,
-        buttons,
-        bars,
-        width=1080,
-        height=600,
-        background_color=(255, 255, 255),
-    ):
-        # background color defaults to white
-        pygame.init()
-        self.window = pygame.display.set_mode(size=(width, height))
-        self.window.fill(background_color)
-        self.width = width
-        self.height = height
-        self.background_color = background_color
+    def __init__(self, screen, navbar, buttons, bars):
+        self.screen = screen
         self.navbar = navbar
-        self.navbar.draw()
         self.buttons = buttons
         self.bars = bars
+        self.navbar.draw(self.screen)
         for button in self.buttons:
-            button.draw()
+            button.draw(self.screen)
         for bar in self.bars:
-            bar.draw()
+            bar.draw(self.screen)
         pygame.display.flip()
 
     def get_clicked_element(self):
