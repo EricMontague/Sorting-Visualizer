b9c11d5bb80511a037b88c024982d9bf401d8357
Eric Montague <eric.g.montague@gmail.com>
2020-06-13 23:42:05 -0400

    Added clicked() method to all buttons. Also, changed how the draw methods are implemented to use pygame.draw.rect instead of surface.blit. Lastly, added properties for determining the center of the InputStepperButton

--- visualizer/buttons.py
+++ visualizer/buttons.py
@@ -2,12 +2,16 @@
 
 # https://inventwithpython.com/blog/2012/10/30/designing-a-button-ui-module-for-pygame/
 import pygame
-# Buttons: 
+from visualizer.widget_types import WidgetType
+
+
+# Buttons:
 # "Reset"
 # "Sort!"
 
 # "Speed": button with a triangle in the middle and a number on the side
 # "Array Size": button with a triangle in the middle
+
 
 class Button:
     """Class to represent a button."""
@@ -16,20 +20,20 @@ class Button:
         self.rectangle = pygame.Rect(dimensions)
         self.label = label
         self.background_color = background_color
+        self.type = WidgetType.BUTTON
 
     def draw(self, surface):
         """Draw the button."""
-        button_surface = self._get_surface()
-        surface.blit(self.rectangle, button_surface)
-        self.label.draw(button_surface, self.rectangle.center)
+        pygame.draw.rect(surface, self.background_color, self.rectangle)
+        self.label.draw(surface, self.rectangle.center)
 
     def undraw(self):
         """Undraw the button."""
         pass
 
-    def clicked(self):
+    def clicked(self, mouse_position):
         """Return True if the user clicked the button."""
-        pass
+        return self.rectangle.collidepoint(mouse_position)
 
     def activate(self):
         """Highlight this button to show that it has been
@@ -53,9 +57,8 @@ class Button:
     def _get_surface(self):
         """Return the rectangular area of the navbar's area as
         a pygame Surface object."""
-        return pygame.Surface(self.rectangle.width, self.rectangle.height)
+        return pygame.Surface(size=(self.rectangle.width, self.rectangle.height))
 
-    
 
 # https://www.nngroup.com/articles/input-steppers/
 class InputStepperButton:
@@ -67,17 +70,34 @@ class InputStepperButton:
         self.points = points
         self.background_color = background_color
         self.line_width = line_width
-        
+        self.type = WidgetType.BUTTON
+
     def draw(self, surface):
         """Draw the button onto the given surface."""
         self.triangle = pygame.draw.polygon(
-            surface, 
-            self.background_color, 
-            self.points, 
-            self.line_width
+            surface, self.background_color, self.points, self.line_width
         )
-        
-    
 
+    def clicked(self, mouse_position):
+        """Return True if the button was clicked, else return False."""
+        return self.triangle.collidepoint(mouse_position)
 
-    
+    @property
+    def center(self):
+        """Return the center of the button as a tuple containing
+        the center's x and y coordinates.
+        """
+        return (self.center_x, self.center_y)
+
+    @property
+    def center_x(self):
+        """Return the x-coordinate of the button's centerpoint."""
+        return max(self.points[0])
+
+    @property
+    def center_y(self):
+        """Return the y-coordinate of the buttons' centerpoint."""
+        y_coords = set()
+        for point in self.points:
+            y_coords.add(point[1])
+        return sum(y_coords) // len(y_coords)
