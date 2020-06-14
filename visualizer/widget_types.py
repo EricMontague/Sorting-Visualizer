a005ca575dec273f566df1a8617c30ee9956bce4
Eric Montague <eric.g.montague@gmail.com>
2020-06-13 23:35:48 -0400

    created module for widget types

---
+++ visualizer/widget_types.py
@@ -0,0 +1,13 @@
+"""This module contains a class responsible for holding the
+different widget types.
+"""
+
+
+class WidgetType:
+    """Class to represent a type of widget."""
+
+    LABEL = 0
+    BUTTON = 1
+    BAR = 2
+    INPUT_STEPPER = 3
+
