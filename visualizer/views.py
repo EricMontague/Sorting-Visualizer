9f2b48e1c76a2fb8e35c84d4f436c1ebae100d85
Eric Montague <eric.g.montague@gmail.com>
2020-06-13 23:37:38 -0400

    removed quit method and added implementation for get_clicked_element() method

--- visualizer/views.py
+++ visualizer/views.py
@@ -2,6 +2,8 @@
 
 
 import pygame
+import sys
+from visualizer.widget_types import WidgetType
 
 
 class SortingVisualizerGUI:
@@ -21,7 +23,16 @@ class SortingVisualizerGUI:
 
     def get_clicked_element(self):
         """Return the element on the screen that was clicked by the user."""
-        pass
+        elements = self.navbar.elements + self.buttons
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    position = pygame.mouse.get_pos()
+                    for element in elements:
+                        if element.clicked(position):
+                            return element, position
 
     def update(self, element):
         """Redraw the given element on the screen."""
@@ -33,21 +44,19 @@ class SortingVisualizerGUI:
 
     def swap(self, bar_one, bar_two):
         """Swap both of the given bars' positions."""
-        #both bars will be red when this method is called.
-        #first, swap them
-        #next, turn them green to indicate they've been swapped
+        # both bars will be red when this method is called.
+        # first, swap them
+        # next, turn them green to indicate they've been swapped
         pass
 
     def sorting_finish(self):
         """Change the colors of all of the bars to indicate
         that they are sorted.
         """
-        #change them to green first and then to purple
+        # change them to green first and then to purple
         pass
 
-    def quit(self):
-        """Return True if the user has closed the pygame window."""
-        for event in pygame.event.get(eventtype=pygame.QUIT):
-            if event is not None:
-                return True
-        return False
+    def reset(self):
+        """Reset the GUI to its original state."""
+        pass
+
