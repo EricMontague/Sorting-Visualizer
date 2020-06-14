09654c930d4f4b75bfe78d32e675cc0565260915
Eric Montague <eric.g.montague@gmail.com>
2020-06-13 23:43:38 -0400

    changed how draw method is implemented to use pygame.draw.rect instead of surface.blit

--- visualizer/labels.py
+++ visualizer/labels.py
@@ -1,14 +1,18 @@
 """This module contains the class responsible for defining a label."""
 
 
+from visualizer.widget_types import WidgetType
+
+
 class Label:
     """Class to represent a label."""
 
     def __init__(self, font, text, font_color, antialias=True, background=None):
         self.font = font
         self.font_color = font_color
-        self.text = self.font.render(text, antialias, font_color, background=background)
-        
+        self.text = self.font.render(text, antialias, font_color, background)
+        self.type = WidgetType.LABEL
+
     def draw(self, surface, center):
         """Draw the label on the given surface."""
         self.center = center
@@ -19,13 +23,17 @@ class Label:
         """Undraw the label."""
         pass
 
+    def clicked(self, mouse_position):
+        """Return True if the label was clicked, else return False."""
+        text_surface = self._get_surface(self.center)
+        return text_surface.collidepoint(mouse_position)
+        
+
     @property
     def name(self):
         """Return the label's text."""
         pass
-    
+
     def _get_surface(self, center):
         """Return the rectangular area of the label's surface as a pygame Surface object."""
-        return self.font.get_rect(center=center)
-        
-        
+        return self.text.get_rect(center=center)
